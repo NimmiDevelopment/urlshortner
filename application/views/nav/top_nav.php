7e6996e9a8d05ce99b332778404b01d3d008387b
Nimmitha Vidyathilaka <nimmideveloper@gmail.com>
2016-03-20 15:05:31 +0900

    Create view top_nav.ph and controller create.php

---
+++ application/views/nav/top_nav.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Project Name: urlshortner
+ * Created by PhpStorm.
+ * Author: Nimmitha Vidyathilaka
+ * Email: nimmidevelopment@gmail.com
+ * Date: 3/20/16
+ * Time: 2:54 PM
+ */
+?>
+<!-- Fixed navbar -->
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><?php echo $this->lang->line('system_system_name'); ?></a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><?php echo anchor('create', 'Create') ; ?></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</div>
+
+<div class="container theme-showcase" role="main">
